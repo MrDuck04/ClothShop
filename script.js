d9c042cef61dab748a99690c19c6612dc0a115cf
Abror Mahir <abror.mahir@g.bracu.ac.bd>
2026-01-04 23:34:38 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const shopBtn = document.getElementById("shopNowBtn");
+
+    shopBtn.addEventListener("click", () => {
+        console.log("Shop Now clicked");       
+    });
+});
